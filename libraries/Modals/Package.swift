6d6d37dc9ebd00de46ceb38dbb3ac2272aa2ff65
John Biggs <john.biggs@proton.ch>
2024-09-09 15:02:28 +0200

    fix: make modals only available for tvOS 17+

--- libraries/Modals/Package.swift
+++ libraries/Modals/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,8 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [
         .iOS(.v15),
-        .macOS(.v11)
+        .macOS(.v11),
+        .tvOS(.v17)
     ],
     products: [
         .library(
